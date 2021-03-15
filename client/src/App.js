a676066048e9ddee52b6eb1e3ec59970ed6d766a
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2021-03-15 10:23:29 +0700

    Update front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 import { Button } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "./actions/posts";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello</h1>
       <Button color="primary" variant="contained">
         I am a button
       </Button>
+      <div>{JSON.stringify(posts)}</div>
     </div>
   );
 }
