32cc94a07b587eddd2a21b197d5daa7d77708c4d
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2021-03-15 10:06:37 +0700

    Update frontend

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,1 +1,13 @@
-import {FETCH_ALL} from 
+import { FETCH_ALL } from "../constants/actionTypes";
+
+import * as api from "../api/index.js";
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
